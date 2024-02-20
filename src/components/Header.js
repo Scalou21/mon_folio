2dad1678e6609ad27454ab389492be3812c57721
Scalou21 <jerome.rouchy@hotmail.fr>
2024-02-20 18:21:15 +0100

    Ajout des dossiers Components avec Header.js (logo + nav list) et Home.js (h1 + photo homepage) et assets contenant logo.png et photo_home.jpg.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export function Header(){
+    return(
+        <header>
+            <img src="../../assets/logo.png" alt="my logo" />
+            <ul>
+                <li>à propos</li>
+                <li>compétences</li>
+                <li>contact</li>
+            </ul>
+        </header>
+    );
+}
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+export function Home(){
+    return(
+        <><div>
+            <h1>Jérôme Rouchy <span>Designer Web</span></h1>
+        </div>
+        <div>
+            <img src="../../assets/photo_home.jpg" alt="Jérôme Rouchy" />
+            <h2>FOLIO</h2>
+        </div></>
+    );
+}
