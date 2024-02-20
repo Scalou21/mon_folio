1b3c4e52bd17f25b01e383d928afe0990ff7fd8d
Khylick <contact.flambot@gmail.com>
2024-02-20 18:36:56 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
+{/* import { Fragment } from "react"; */}
+
 export function Home(){
     return(
+        {/* <Fragment> ) à la place de cette merde <>  */}
         <><div>
             <h1>Jérôme Rouchy <span>Designer Web</span></h1>
         </div>
@@ -7,5 +10,6 @@ export function Home(){
             <img src="../../assets/photo_home.jpg" alt="Jérôme Rouchy" />
             <h2>FOLIO</h2>
         </div></>
+        {/* </Fragment> ) à la place de cette merde </>  */}
     );
 }
