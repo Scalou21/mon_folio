ecd962ee405dbc24638d518bc7d3b7597bcf874d
Scalou21 <jerome.rouchy@hotmail.fr>
2024-02-21 15:39:33 +0100

    App.js modifié

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import { Header } from ".//components/Header.js";
+import { Home } from ".//components/Home.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Salut Aomame !
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+
+      <Header />
+
+      <Home />
+
+    </Fragment>
+
   );
 }
 
