3a0512073d57c529a6a73d5fd88f58583e97f145
Scalou21 <jerome.rouchy@hotmail.fr>
2024-03-01 18:01:26 +0100

    Makes the logo clickable and take you back to the home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header>
+      <Link to="/">
       <img src="../../assets/logo.png" alt="my logo" />
+      </Link>
 
       <Navbar />
     </header>
