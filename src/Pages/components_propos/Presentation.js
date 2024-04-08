db3dcdd5e198efeab0f2f0d9a5fd8cf59e46a162
Scalou21 <jerome.rouchy@hotmail.fr>
2024-04-08 18:02:50 +0200

    Integrates scss/css for Presentation component
    Intégration du css pour le composant Presentation.js dans la page à
    propos.
    
    Responsive à faire...
    
    [Ticket: X]

--- src/Pages/components_propos/Presentation.js
+++ src/Pages/components_propos/Presentation.js
@@ -14,28 +14,27 @@ export default function Presentation() {
         </div>
       </section>
       <section className="section is-flex is-justify-content-space-between">
-        <aside className="w-50pct">
-          <h2 className="has-text-weight-semibold fs-6 is-relative">
+        <aside className="w-50pct is-flex">
+        <h2 className="has-text-weight-semibold fs-6 w-25 absolute">
             On se tutoie ?
           </h2>
-        </aside>
-
-        <div className="w-50pct">
-          <div className="is-flex">
+        <div className="is-flex w-100pct is-justify-content-flex-end pright-5">
             <img
               src="../../assets/photo_pres.jpg"
               alt="Jérôme Rouchy"
             />
           </div>
-        </div>
+        
+         
+        </aside>
 
-        <article className="w-45 fs-2-25">
-          <h3>Petite présentation :</h3>
-          <p>
+        <article className="is-flex w-50pct is-flex-direction-column is-justify-content-center">
+          <h3 className="has-text-weight-semibold is-size-2">Petite présentation :</h3>
+          <p className="pt-6 fs-2-25">
             Je m'appelle Jérôme Rouchy, un passionné du numérique âgé de 31 ans
             et résidant aux abords de Dijon.
           </p>
-          <p>
+          <p className="pt-6 fs-2-25">
             Je suis titulaire du permis B et je possède également mon propre
             véhicule.
           </p>
